6ac2d9c1e6e736b29ec431ac011d883b0a123bdb
Justin Hammack <justin@pungle.me>
2011-04-25 13:22:03 -0700

    v3.278 - missed adding these files last commit

---
+++ contact.php
@@ -0,0 +1,68 @@
+<!doctype html>  
+
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ --> 
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+
+<head>
+    <title>pungle - contact</title>
+    <meta name="description" content="Contact us? What, you want to chat? Mmmkay.">
+    <meta name="author" content="pungle.me">
+<?php require("include/head.php"); ?>
+
+    <!-- !!MUST SET THIS IMPORTANT!! -->
+    <link rel="canonical" href="http://pungle.me/about-us/contact/" />
+</head>
+
+<body>
+<?php require("include/nav.php"); ?>
+
+    
+    <div id="content" class="clearfix">
+       
+        <div class="row">
+            <div class="col col_16">
+            	<h2>Contact</h2>            	
+        	</div>
+        </div>
+        
+        <div class="row">
+        	
+        	<div class="col col_16 align_center">
+        		<img src="/images/1060x1.png" title="Ouch!" width="940" height="1" />
+        	</div>
+        	
+        </div>
+        
+    	<div class="row">
+            <div class="col col_16">
+            	<p>
+          			<a class="fbButtonBig" href="http://www.facebook.com/pungleDOTorg" target="_blank" title="Connect with punglers on facebook.">Facebook</a>
+          			<a class="twButtonBig" href="http://twitter.com/JustinHammack" target="_blank" title="Follow our founder on Twitter.">Twitter</a>          		
+          			<a class="emButtonBig" href="mailto:contact@pungle.me" title="Email us!">Email</a>
+          		</p>
+            	<br/>
+          		<h3>phone: 206-550-6349</h3>
+          		<br/>
+          		<h3>address:</h3>
+          		<p style="margin-left:50px;">
+          		    pungle.me<br/>
+          		    1823 Terry Ave Unit 715<br/>
+          		    Seattle, WA 98101
+          		</p>
+          		<br/><br/>
+          		<iframe src="https://spreadsheets.google.com/embeddedform?formkey=dHoyVFBpZ1V3YzVfT3U2OEpkLVdjV1E6MQ" width="760" height="850" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+        	</div>
+        </div>
+        
+        
+    </div>
+
+<?php require("include/footer.php"); ?>
+    
+</body>
+
+</html>
---
+++ team.php
@@ -0,0 +1,169 @@
+<!doctype html>  
+
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ --> 
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+
+<head>
+    <title>pungle - our team</title>
+    <meta name="description" content="Discover the team at pungle.">
+    <meta name="author" content="pungle.me">
+<?php require("include/head.php"); ?>
+
+    <!-- !!MUST SET THIS IMPORTANT!! -->
+    <link rel="canonical" href="http://pungle.me/about-us/our-team/" />
+</head>
+
+<body>
+<?php require("include/nav.php"); ?>
+
+    
+    <div id="content" class="clearfix">
+        
+        <div class="row">
+            <div class="col col_16">
+            	<h2>Our Team</h2>            	
+        	</div>
+        </div>
+        
+        <div class="row">
+        	
+        	<div class="col col_16 align_center">
+        		<img src="/images/1060x1.png" title="Ouch!" width="940" height="1" />
+        	</div>
+        	
+        </div>
+        
+        <div class="row">
+        	
+        	<div class="col col_16">
+        		<p><strong><em><a href="http://en.wikipedia.org/wiki/Wikipedia:Too_long;_didn't_read" target="_blank" title="Too long; didn't read. Gimme a one sentence summary.">TL;DR</a> ex-gamers founded pungle to make up for their wretched ways.</em></strong></p>
+        	</div>
+        	
+        </div>
+        
+    <!-- Justin Hammack -->
+		<div class="row">
+			<div class="col col_6">
+				<img src="/images/justin_hammack.jpg" title="Justin Hammack" width="340" height="210" />
+			</div>
+			<div class="col col_10">
+				<h3>Justin Hammack (<a href="http://about.me/justinhammack" target="_blank" title="Details on http://about.me">contact</a>)</h3>
+				<p>
+					<em>Lead developer & pungle evangelist. Scotch drinking anti-socialite.</em><br/>
+					<br/>
+					I'm an entrepreneur, vegetarian foodie, coffee addict, ex-pro PC gamer, lover of cult movies, and driver of insane turbo-2.0L. I'm based out of Seattle, where the pungle secrets are kept. Yes, I'd love to grab some happy hour, glad you asked.
+				</p>
+			</div>
+		</div>
+		
+		<!-- Ben Andrews -->
+		<div class="row">
+			<div class="col col_6">
+				<img src="/images/ben_andrews.jpg" title="Ben Andrews" width="340" height="210" />
+			</div>
+			<div class="col col_10">
+				<h3>Ben Andrews (<a href="http://about.me/bandrews" target="_blank" title="Details on http://about.me" />contact</a>)</h3>
+				<p><em>Director of marketing & propaganda. Puts the sass in sassy.</em><br/>
+					<br/>
+					I grew up playing the original Nintendo. My generation invented gaming, sorry, you can't top that. I have an elementary school degree in Oregon Trail. You know what, forget this, I can't be bothered to talk too much about myself, because frankly I'm busy being unbelievably awesome. How awesome am I? <a href="http://www.google.com/images?q=unicorns" target="_blank">THIS AWESOME</a>
+				</p>
+			</div>
+		</div>
+		
+		<!-- Madeline Puckette -->
+		<div class="row">
+			<div class="col col_6">
+				<img src="/images/madeline_puckette.jpg" title="Madeline Puckette" width="340" height="210" />
+			</div>
+			<div class="col col_10">
+				<h3>Madeline Puckette (<a href="http://about.me/madelinep" target="_blank" title="Details on http://about.me">contact</a>)</h3>
+				<p><em>Master designer. Professional socialite.</em><br/>
+					<br/>
+					In addition to being a designer, I'm an avid FPS gamer and an electro-pop singer. My vocals are featured on a few awesome electronica albums. I also drink wine professionally. Recently, I was awarded a wine sommelier certificate by CoM and placed runner-up in a national blind tasting competition. Salut!
+				</p>
+			</div>
+		</div>
+		
+		<!-- Chad Wasser -->
+		<div class="row">
+			<div class="col col_6">
+				<img src="/images/chad_wasser.jpg" title="Chad Wasser" width="340" height="210" />
+			</div>
+			<div class="col col_10">
+				<h3>Chad Wasser (<a href="mailto:contact@pungle.me">email</a>)</h3>
+				<p><em>Ministry of Satire, Parody, & Civic Discourse</em><br/>
+					<br/>
+					I like abstract strategy mechanics, amateur woodworking, recreational nonviolent conflict, invisible forces, and double short americanos. I live in Seattle, where I touch really fragile things with kid gloves for a living.
+				</p>
+			</div>
+		</div>
+		
+		<!-- Jourdan McGinn -->
+		<div class="row">
+			<div class="col col_6">
+				<img src="/images/jourdan_mcginn.jpg" title="Jourdan McGinn" width="340" height="210" />
+			</div>
+			<div class="col col_10">
+				<h3>Jourdan McGinn (<a href="mailto:contact@pungle.me">email</a>)</h3>
+				<p><em>Lowly intern. Jack of some trades. I've actually been to Africa!</em><br/>
+					<br/>
+					An avid tea drinker who is wildly addicted to strawberry jam. I am afraid of fish and driving over forty but live an uncharacteristically risky life on the side, travelling the dark continent alone and escaping a near kidnapping in West Africa. I am a technologically inept Seattlite working for a tech startup, a bit of a conundrum, no?
+				</p>
+			</div>
+		</div>
+		
+		<!-- Micah Huerta -->
+		<div class="row">
+			<div class="col col_6">
+				<img src="/images/micah_huerta.jpg" title="Micah Huerta" width="340" height="210" />
+			</div>
+			<div class="col col_10">
+				<h3>Micah Huerta (<a href="mailto:micah@pungle.me">email</a>)</h3>
+				<p><em>Back end developer. East coast liaison. Will min/max your dinner order.</em><br/>
+					<br/>
+					I love food, wine, PC games and I'm an extremely talented musician. (ugh)
+				</p>
+			</div>
+		</div>
+		
+		<!-- Katie Andrews -->
+		<div class="row">
+			<div class="col col_6">
+				<img src="/images/katie_andrews.jpg" title="Katie Andrews" width="340" height="210" />
+			</div>
+			<div class="col col_10">
+				<h3>Katie Andrews (<a href="mailto:contact@pungle.me">email</a>)</h3>
+				<p><em>Grass roots advocate. Organizational expert. Good luck charm.</em><br/>
+					<br/>
+					No one ever wrote a description for me. Fear my wrath.
+				</p>
+			</div>
+		</div>
+		
+		<!-- Mourad Salji 
+		<div class="row">
+			<div class="col col_5">
+				<img src="/images/aboutus_jbh.jpg" title="Mourad Salji" width="340" height="210" />
+			</div>
+			<div class="col col_8">
+				<h3>Mourad Salji (<a href="mailto:contact@pungle.me">email</a>)</h3>
+				<p><em>International ambassador. The Canadian connection. Male model.</em><br/>
+					<br/>
+					Need your cheese here.					
+				</p>
+			</div>
+		</div>
+		-->		
+		
+		
+    </div>
+
+<?php require("include/footer.php"); ?>
+    
+</body>
+
+</html>
