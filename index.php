eb3e873d94e941234adbe61e02f1e54952a38e20
Justin Hammack <justin@pungle.org>
2011-09-22 18:07:54 -0700

    Updated Front Page, Added Extensions, Modified Style

--- index.php
+++ index.php
@@ -28,29 +28,28 @@
             display: table;
         }
         
-        #content_wrapper {
-            height: 940px;
+        #content_wrapper {            
             background: none;
+        }
+        
+        #content_wrapper2 {
+          display: block;    
+          background: none;
+          background-color: #ccebff;
+          padding: 0;
+          width: 100%;
         }
         
         #content {
-            background: none;
-            -webkit-box-shadow: none;
+          background: none;
+          -webkit-box-shadow: none;
 	        -moz-box-shadow: none;
 	        box-shadow: none;
+	        padding-bottom: 0px;
         }
         
 	    #subfooter {display: none;}
-	    #footer_wrapper { margin-top: 135px; background: none; }
-	    
-        #mce-FNAME { margin-top: 5px; width: 293px; }
-        #mce-EMAIL { margin-top: 5px; width: 220px; }
-        
-        .response { font-size: 13px; }
-        
-        .imageBuffer { margin: 26px 0 8px;}
-        
-        #fb-root { margin-top: 8px; }
+
 	</style>
 	
 </head>
@@ -65,76 +64,68 @@
 	        
 	        <div class="col col_8 align_left">
 	            <br/>
-	            <h1>6,709 people agree.</h1><br>
+	            <h1>6,709 people agree.</h1>
 	            <h3>pungle is the easiest way to support causes when you shop online. free.</h3>
-	            <br></br>
-	            <div class="ctabutton">Install App
-	            </div> <br>      
-	        </div>	 
-	        <div class="col col_8 align_right">
-	            <img src="images/thumbsUpCat385x485.png"></img>
-	        </div>
-	    	                   
-	            <!-- <p>
-	                <span style="font: 16px/23px 'helvetica neue', Helvetica, Arial, Verdana, sans-serif;">Use our <a style="color: #0664bb" href="/shop/" title="Shopping Tool">Shopping Tool</a> to turn your<br/> purchases into donations.</span>
-	            </p> -->
-	            
-
-	        
-	       <!-- <div class="col indexForm">
-	            
-	            <!-- Step 1 Facebook 
-	            <img src="/images/PU_step1a.png" width="125" height="30" alt="Step 1" title="Step 1" /><br/>
-                <small>Join our community and grow with us:</small><br/>
-	            <div id="fb-root"></div><script src="http://connect.facebook.net/en_US/all.js#appId=179859935402175&amp;xfbml=1"></script><fb:like href="https://www.facebook.com/pungleDOTorg" send="false" width="300" show_faces="true" font="arial"></fb:like>
-	            
-	            <!-- Step 2 Shopping Tool 
-	            <img src="/images/PU_step2b.png" width="192" height="30" alt="Step 2" title="Step 2" class="imageBuffer" />
-	            <a style="margin: 0 75px;" class="dribbble" href="/how-it-works/" title="How it Works!">Start ></a>
 	            <br/>
-	            
-	            <img style="margin-top: 8px;" src="/images/300x5.png" width="300" height="5" alt="split" />
-	            <!-- Optional 
-	            <img style="margin-top: 8px;" src="/images/PU_step3a.png" width="250" height="33" alt="Step 3" title="Step 3" /><br/>
-	            <small>We're working out the kinks and looking for a few users to try our experimental features. If that's you, enter your email below and we'll be in touch.</small>
-	            
-	            <!-- Begin MailChimp Signup Form 
-	            
-                    <script type="text/javascript">
-                    // delete this script tag and use a "div.mce_inline_error{ XXX !important}" selector
-                    // or fill this in and it will be inlined when errors are generated
-                    var mc_custom_error_style = 'background: #FFEEEE none repeat scroll 0% 0%; float: left; z-index: 1; -moz-background-clip: -moz-initial; -moz-background-origin: -moz-initial; -moz-background-inline-policy: -moz-initial; color: #FF0000;';
-                    </script>
-                    
-                    <div id="mc_embed_signup">
-                        <form action="http://pungle.us2.list-manage1.com/subscribe/post?u=49a938eaff3e75d15622b305b&amp;id=4705aa678e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
-                            <fieldset>
-                                
-                                <!-- <div class="mc-field-group">
-                                    <input type="text" placeholder="Full Name" value="" name="FNAME" class="" id="mce-FNAME">
-                                </div> 
-                                <div class="mc-field-group">
-                                    <input type="text" placeholder="Email Address" value="" name="EMAIL" class="required email" id="mce-EMAIL">                                    
-                                    <input type="submit" value="Invite" name="subscribe" id="mc-embedded-subscribe" class="signup">
-                                </div>
-                		        <div id="mce-responses">
-                		            
-                		            <div class="response" id="mce-error-response" style="display:none"></div>
-                		            <div class="response" id="mce-success-response" style="display:none"></div>
-                		            
-                		        </div>                		        
-                		        
-                    	    </fieldset>	
-                    	    <!-- <a href="#" id="mc_embed_close" class="mc_embed_close">Close</a> 
-                        </form>
-                    </div>
-                
-                <!--End mc_embed_signup
-	            
-	        </div> 
-		</div> -->
+	            <a id="extInstall" class="ctabutton" href="#">Install App</a>
+	            <br/>
+	        </div>	 
+	        
+	        <div class="col col_8 align_right">
+	            <img src="images/thumbsUpCat385x485.png" />
+	        </div>
+	        
+	    </div>
+	</div>
+</div> <!-- ends content wrapper hidden in header -->
+
+<div id="content_wrapper2">
+  <div id="content" class="clearfix">
+    
+		<div class="row">
+		  <div class="col col_16">
+		    <h3>Top Causes</h3>
+		    <p>Install the app and select a <b>cause</b>.</p>
+		  </div>
+		</div>
 		
-		<div class="row" style="background-color: #FFF">
+		<div class="row">
+		  <div class="col col_3">
+		    <p><img src="/images/ca_edu_ico64x64.png" /></p>
+		    <p><b>Education</b><br/>
+		      Get students the tools they need to learn.
+		    </p>
+		  </div>
+		  <div class="col col_3">
+		    <p><img src="/images/ca_tech_ico64x64.png" /></p>
+		    <p><b>Technology</b><br/>
+		      Development of life-changing technology.
+		    </p>
+		  </div>
+		  <div class="col col_3">
+		    <p><img src="/images/ca_water_ico64x64.png" /></p>
+		    <p><b>Potable Water</b><br/>
+		      Build sustainable fresh water sources.
+		    </p>
+		  </div>
+		  <div class="col col_3">
+		    <p><img src="/images/ca_nature_ico64x64.png" /></p>
+		    <p><b>Nature</b><br/>
+		      Increase forest size & natural landscapes.
+		    </p>
+		  </div>
+		  <div class="col col_3">
+		    <p><img src="/images/ca_energy_ico64x64.png" /></p>
+		    <p><b>Clean Energy</b><br/>
+		      Offset your carbon footprint.
+		    </p>
+		  </div>
+		  <div class="col col_1">
+		    <br/>		    
+		  </div>
+		</div>
+		
+		<div class="row">
 		  <div class="col col_16">
 		    <h3>Answers</h3>
 		    <p>First time here? You're probably wondering...</p>
@@ -146,42 +137,9 @@
 		  </div>
 		</div>
 		
-	    <!-- introducing pungle 
-		<div class="row">
-			<div class="col col_6">
-				<p><a href="#" class="readMoreImage" title="Learn more about pungle!"><img src="/images/recruits-wanted.jpg" alt="Recruits wanted!" width="340" height="361"/></a></p>
-				
-			</div>
-			<div class="col col_10 align_center">
-				<p><a href="#" title="Save the world!"><img src="/images/save-the-world.jpg" alt="Save the World" width="580" height="361" /></a></p>
-			</div>
-		</div> -->
-		
-		<!--<div class="col col_7 align_right indexMission">
-	            Maximize your buying power. Pungle turns ordinary purchases into high<br/> impact donations to non-profits at no cost to you. <strong><i>Join today. Save the world.</i></strong>
-	        </div> -->
-	        
-		<!-- <p><br/>Shop at pungle to transform ordinary purchases into pure awesome. Build a better world, mock the incompetent & feel good about yourself! -->
-				    <!--Pungling is responsible shopping; where your puchases help make the world a better place.--><!--100% of profits go to charity.--><!-- Free to use. -->
-				    <!-- Free to use. Most major stores supported.</b><br/><br/> -->
-				    <!-- <span class="readMoreButton left" title="Scrolling down the page is exhausting, let us help.">Read More</span><a class="goToAnvilButton left" href="/anvil/" title="100% free to use, pungling for charity is easy!">The Anvil</a>-->
-		<!-- </p> -->
-		
-		<!-- social media hangar 
-		<div class="addthis_toolbox addthis_default_style smHanger">
-			<a class="addthis_button_facebook_like" fb:like:layout="button_count" addthis:url="http://www.facebook.com/pungleDOTorg"></a>
-			<a class="addthis_button_tweet" addthis:url="http://pungle.me" addthis:title="hey @JustinHammack, I just pungled a little."></a>			
-		</div> -->
-		
-        
-		<!-- splitsies content 
-		<div class="row" id="whatispungle">
-            <div class="col col_16 align_center">
-                <img src="/images/whatispungleme.png" width="940" height="40" title="i just pungled a little.." />
-            </div>
-        </div> -->
-	
 	</div>
+	
+</div>
 
 <?php require("include/footer.php"); ?>
 
@@ -192,185 +150,27 @@
 	
 	<script type="text/javascript">
 	    $(document).ready(function(){
-            
-        $('.readMoreButton, .readMoreImage').click(function(){
-				$('html, body').animate({
-					scrollTop: $("#whatispungle").offset().top
-				}, 800);
-			});
-			
-	    });
-	</script> 
-    
-	<!-- put this here because it can stall out like a fighter jet going vertical 
-	<script type="text/javascript">var addthis_config = {"data_track_clickback":true};</script>
-	<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#username=pungle"></script>
-    -->
-    
-    <!-- mail chimp script -->
-    <script type="text/javascript">
-        var fnames = new Array();var ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';
-        try {
-            var jqueryLoaded=jQuery;
-            jqueryLoaded=true;
-        } catch(err) {
-            var jqueryLoaded=false;
-        }
-        var head= document.getElementsByTagName('head')[0];
-        if (!jqueryLoaded) {
-            var script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js';
-            head.appendChild(script);
-            if (script.readyState && script.onload!==null){
-                script.onreadystatechange= function () {
-                      if (this.readyState == 'complete') mce_preload_check();
-                }    
-            }
-        }
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = 'http://downloads.mailchimp.com/js/jquery.form-n-validate.js';
-        head.appendChild(script);
-        var err_style = '';
-        try{
-            err_style = mc_custom_error_style;
-        } catch(e){
-            err_style = 'margin: 1em 0 0 0; padding: 1em 0.5em 0.5em 0.5em; background: ERROR_BGCOLOR none repeat scroll 0% 0%; font-weight: bold; float: left; z-index: 1; width: 80%; -moz-background-clip: -moz-initial; -moz-background-origin: -moz-initial; -moz-background-inline-policy: -moz-initial; color: ERROR_COLOR;';
-        }
-        var head= document.getElementsByTagName('head')[0];
-        var style= document.createElement('style');
-        style.type= 'text/css';
-        if (style.styleSheet) {
-          style.styleSheet.cssText = '.mce_inline_error {' + err_style + '}';
+        
+        if($.browser.name == 'chrome') {
+          $("#extInstall").attr('href', 'http://pungle.me/extensions/chrome1.0.5.crx');
+        } else if ($.browser.name == 'firefox') {
+          $("#extInstall").attr('href', 'http://pungle.me/extensions/firefox0.5.24.xpi');
         } else {
-          style.appendChild(document.createTextNode('.mce_inline_error {' + err_style + '}'));
-        }
-        head.appendChild(style);
-        setTimeout('mce_preload_check();', 250);
-        
-        var mce_preload_checks = 0;
-        function mce_preload_check(){
-            if (mce_preload_checks>40) return;
-            mce_preload_checks++;
-            try {
-                var jqueryLoaded=jQuery;
-            } catch(err) {
-                setTimeout('mce_preload_check();', 250);
-                return;
-            }
-            try {
-                var validatorLoaded=jQuery("#fake-form").validate({});
-            } catch(err) {
-                setTimeout('mce_preload_check();', 250);
-                return;
-            }
-            mce_init_form();
-        }
-        function mce_init_form(){
-            jQuery(document).ready( function($) {
-              var options = { errorClass: 'mce_inline_error', errorElement: 'div', onkeyup: function(){}, onfocusout:function(){}, onblur:function(){}  };
-              var mce_validator = $("#mc-embedded-subscribe-form").validate(options);
-              $("#mc-embedded-subscribe-form").unbind('submit');//remove the validator so we can get into beforeSubmit on the ajaxform, which then calls the validator
-              options = { url: 'http://pungle.us2.list-manage1.com/subscribe/post-json?u=49a938eaff3e75d15622b305b&id=4705aa678e&c=?', type: 'GET', dataType: 'json', contentType: "application/json; charset=utf-8",
-                            beforeSubmit: function(){
-                                $('#mce_tmp_error_msg').remove();
-                                $('.datefield','#mc_embed_signup').each(
-                                    function(){
-                                        var txt = 'filled';
-                                        var fields = new Array();
-                                        var i = 0;
-                                        $(':text', this).each(
-                                            function(){
-                                                fields[i] = this;
-                                                i++;
-                                            });
-                                        $(':hidden', this).each(
-                                            function(){
-                                                if (fields.length == 2) fields[2] = {'value':1970};//trick birthdays into having years
-                                            	if ( fields[0].value=='MM' && fields[1].value=='DD' && fields[2].value=='YYYY' ){
-                                            		this.value = '';
-        									    } else if ( fields[0].value=='' && fields[1].value=='' && fields[2].value=='' ){
-                                            		this.value = '';
-        									    } else {
-        	                                        this.value = fields[0].value+'/'+fields[1].value+'/'+fields[2].value;
-        	                                    }
-                                            });
-                                    });
-                                return mce_validator.form();
-                            }, 
-                            success: mce_success_cb
-                        };
-              $('#mc-embedded-subscribe-form').ajaxForm(options);      
-              
-            });
-        }
-        function mce_success_cb(resp){
-            $('#mce-success-response').hide();
-            $('#mce-error-response').hide();
-            if (resp.result=="success"){
-                $('#mce-'+resp.result+'-response').show();
-                $('#mce-'+resp.result+'-response').html(resp.msg);
-                $('#mc-embedded-subscribe-form').each(function(){
-                    this.reset();
-            	});
-            } else {
-                var index = -1;
-                var msg;
-                try {
-                    var parts = resp.msg.split(' - ',2);
-                    if (parts[1]==undefined){
-                        msg = resp.msg;
-                    } else {
-                        i = parseInt(parts[0]);
-                        if (i.toString() == parts[0]){
-                            index = parts[0];
-                            msg = parts[1];
-                        } else {
-                            index = -1;
-                            msg = resp.msg;
-                        }
-                    }
-                } catch(e){
-                    index = -1;
-                    msg = resp.msg;
-                }
-                try{
-                    if (index== -1){
-                        $('#mce-'+resp.result+'-response').show();
-                        $('#mce-'+resp.result+'-response').html(msg);            
-                    } else {
-                        err_id = 'mce_tmp_error_msg';
-                        html = '<div id="'+err_id+'" style="'+err_style+'"> '+msg+'</div>';
-                        
-                        var input_id = '#mc_embed_signup';
-                        var f = $(input_id);
-                        if (ftypes[index]=='address'){
-                            input_id = '#mce-'+fnames[index]+'-addr1';
-                            f = $(input_id).parent().parent().get(0);
-                        } else if (ftypes[index]=='date'){
-                            input_id = '#mce-'+fnames[index]+'-month';
-                            f = $(input_id).parent().parent().get(0);
-                        } else {
-                            input_id = '#mce-'+fnames[index];
-                            f = $().parent(input_id).get(0);
-                        }
-                        if (f){
-                            $(f).append(html);
-                            $(input_id).focus();
-                        } else {
-                            $('#mce-'+resp.result+'-response').show();
-                            $('#mce-'+resp.result+'-response').html(msg);
-                        }
-                    }
-                } catch(e){
-                    $('#mce-'+resp.result+'-response').show();
-                    $('#mce-'+resp.result+'-response').html(msg);
-                }
-            }
+          $('#extInstall').click(function() {
+            alert('Sorry, we only support Chrome & Firefox.');
+          });
         }
         
-    </script>
+        
+        $('.readMoreButton, .readMoreImage').click(function(){
+				  $('html, body').animate({
+				    scrollTop: $("#whatispungle").offset().top
+				  }, 800);
+			  });
+			  
+	    });
+	</script>
+
 
 </body>
 
